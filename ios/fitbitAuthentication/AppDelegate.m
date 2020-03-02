6c5afc3f504b2af244d6bd2b02ba7a279b2c880c
Caleb Marcinkovich <cmarcink@us.ibm.com@Calebs-MacBook-Pro.local>
2020-03-02 10:32:25 -0600

    Initialize with auth template

---
+++ ios/fitbitAuthentication/AppDelegate.m
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import "RCTLinkingManager.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    openURL:(NSURL *)url
+    sourceApplication:(NSString *)sourceApplication
+    annotation:(id)annotation {
+      return [RCTLinkingManager
+          application:application
+          openURL:url
+          sourceApplication:sourceApplication
+          annotation:annotation];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
